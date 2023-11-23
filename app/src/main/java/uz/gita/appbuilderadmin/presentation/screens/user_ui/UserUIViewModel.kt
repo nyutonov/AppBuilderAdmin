c692da4f7fa8f83cd85a1ac78391c741e262de59
akmal <akmal19880aliev@gmail.com>
2023-11-23 03:44:23 -0800

    getAllData from realtime database

--- app/src/main/java/uz/gita/appbuilderadmin/presentation/screens/user_ui/UserUIViewModel.kt
+++ app/src/main/java/uz/gita/appbuilderadmin/presentation/screens/user_ui/UserUIViewModel.kt
@@ -4,13 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import uz.gita.appbuilderadmin.domain.repository.Repository
 import javax.inject.Inject
 
 @HiltViewModel
 class UserUIViewModel @Inject constructor(
-    private val direction: UserUIContract.Direction
+    private val direction: UserUIContract.Direction,
+    private val repository: Repository
 ) : UserUIContract.ViewModel, ViewModel() {
     override val uiState = MutableStateFlow(UserUIContract.UIState())
 
@@ -18,6 +22,12 @@ class UserUIViewModel @Inject constructor(
         when (intent) {
             is UserUIContract.Intent.ClickAddComponents -> {
                 viewModelScope.launch { direction.moveToConstructor(intent.name) }
+            }
+
+            is UserUIContract.Intent.LoadData -> {
+                repository.getAllData(intent.name).onEach { list ->
+                    uiState.update { it.copy(components = list) }
+                }.launchIn(viewModelScope)
             }
 
             is UserUIContract.Intent.SetName -> {
