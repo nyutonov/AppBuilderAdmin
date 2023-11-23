2dff4b9e0a5c4bb9349cbcd0e5e705deba53661d
nyutonov <shahriyor787@gmail.com>
2023-11-23 15:22:52 +0500

    .

---
+++ app/src/main/java/uz/gita/appbuilderadmin/presentation/components/TextComponent.kt
@@ -0,0 +1,19 @@
+package uz.gita.appbuilderadmin.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import uz.gita.appbuilderadmin.data.model.ComponentsModel
+
+@Composable
+fun  TextComponent(
+ data:ComponentsModel
+) {
+    Box(modifier = Modifier.fillMaxWidth()){
+     Text(text = data.text, modifier = Modifier.padding(start = 10.dp))
+    }
+}
