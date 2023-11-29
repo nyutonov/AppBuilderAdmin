0a44fbbbfd57dd98c5f0fb322459d20327c72cdc
azizbeklive <otaboevlive@gmail.com>
2023-11-29 20:30:39 +0500

    fix

--- app/src/main/java/uz/gita/appbuilderadmin/presentation/components/RowTextComponent.kt
+++ app/src/main/java/uz/gita/appbuilderadmin/presentation/components/RowTextComponent.kt
@@ -1,17 +1,11 @@
 package uz.gita.appbuilderadmin.presentation.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import uz.gita.appbuilderadmin.data.model.ComponentsModel
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -19,26 +13,16 @@ import uz.gita.appbuilderadmin.data.model.ComponentsModel
 fun RowTextComponent(
     data: ComponentsModel,
     onClick: () -> Unit = {},
-    modifier: Modifier
+    modifier: Modifier,
 ) {
-
-
-    Column {
-        Box(
-            modifier = Modifier
-                .weight(
-                    data.weight
-                )
-        ) {
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Text(
-                    text = data.text,
-                    modifier = modifier
-                        .padding(start = 2.dp)
-                        .align(Alignment.CenterVertically),
-                    color = Color.LightGray,
-                )
-            }
-        }
+    Row(modifier = modifier) {
+        Text(
+            text = data.text,
+//            modifier = Modifier
+//                .align(Alignment.CenterVertically)
+//                .background(Color(0x80FFFFFF))
+//                .padding(start = 2.dp),
+            color = Color.White,
+        )
     }
 }
